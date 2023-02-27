76be87a3678494bb4dabd92f02f77e4b73ec61cf
Anastasiya Markova <a.v.markova00@gmail.com>
2023-02-27 23:41:57 +0300

    add uncorrect solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
 
-    setRange(min, max) {
+    this.min = null;
+    this.max = null;
+    this.guessNum = null;
+  }
 
-    }
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    guess() {
+  guess() {
+    this.guessNum = Math.floor((this.min + this.max) / 2);
+    return this.guessNum;
+  }
 
-    }
+  lower() {
+    this.max = this.guessNum;
+  }
 
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.guessNum;
+  }
 }
-
 module.exports = GuessingGame;
